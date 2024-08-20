535d4d2c0257759bcec51f7be554320e7cf33d00
Khoshang <khoshangkashyap123@gmail.com>
2024-08-20 23:53:32 +0530

    final changes

---
+++ Timeline/script.js
@@ -0,0 +1,14 @@
+const blob = document.querySelector("[data-blob]");
+
+window.addEventListener("mousemove", function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  blob.style.left = `${posX}px`;
+  blob.style.top = `${posY}px`;
+
+  blob.animate({
+    left: `${posX}px`,
+    top: `${posY}px`  
+  }, {duration:500, fill: "forward"})
+}) 
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -19,3 +19,19 @@ var d = setInterval(function () {
         document.getElementById("sec").innerHTML = seconds;
     }
 }, 1000);
+
+const blob = document.querySelector("[data-blob]");
+
+window.addEventListener("mousemove", function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  blob.style.left = `${posX}px`;
+  blob.style.top = `${posY}px`;
+
+  blob.animate({
+    left: `${posX}px`,
+    top: `${posY}px`  
+  }, {duration:500, fill: "forward"})
+})
+
